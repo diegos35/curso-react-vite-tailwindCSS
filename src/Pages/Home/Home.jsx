d98f11a307c08f0e65f7478a2117389bc40f0ec8
Diego-caviedes <diego_1996.c.c@hotmail.com>
2023-06-15 16:15:15 -0500

    use hook useState and UseEffect for implement cards

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,31 @@
+import { useState, useEffect } from 'react';
 import Card from '../../components/Card';
 import Layout from '../../components/Layout';
 
 function Home() {
+  const [products, setProducts] = useState(null);
+
+  useEffect(() => {
+    const apiUrl = 'https://api.escuelajs.co/api/v1';
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${apiUrl}/products`);
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.error(`Oh no, ocurrió un error: ${error}`);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <Layout>
-      Home
-      <Card />
+      <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
+        {products?.map((product) => (
+          <Card key={product.id} {...product} />
+        ))}
+      </div>
     </Layout>
   );
 }
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,22 +1,22 @@
-const Card = () => {
+const Card = ({ price, title, images, category: { name } }) => {
   return (
     <div className='bg-white cursor-pointer w-56 h-60 rounded-lg'>
       <figure className='relative mb-2 w-full h-4/5'>
         <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5'>
-          Electronics
+          {name}
         </span>
         <img
           className='w-full h-full object-cover rounded-lg'
-          src='https://images.pexels.com/photos/17202898/pexels-photo-17202898/free-photo-of-city-street-building-house.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load'
-          alt='headphones'
+          src={images}
+          alt={title}
         />
         <button className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1'>
           +
         </button>
       </figure>
       <p className='flex justify-between'>
-        <span className='text-sm font-light'>Headphones</span>
-        <span className='text-lg font-medium'>$300</span>
+        <span className='text-sm font-light'>{title}</span>
+        <span className='text-lg font-medium'>${price}</span>
       </p>
     </div>
   );
