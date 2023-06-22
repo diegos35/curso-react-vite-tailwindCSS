06dcf4a882b79bec1e3f6a376fcfc5cf7574a4bf
Diego-caviedes <diego_1996.c.c@hotmail.com>
2023-06-22 12:17:24 -0500

    maquetando el product detail y validacion para abrir y cerrar el product detail

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -5,12 +5,18 @@ export const ShoppingCartContext = createContext();
 //recibe hijos
 export const ShoppingCartProvider = ({ children }) => {
   const [count, setCount] = useState(0);
+  const [isProductOpen, setIsProductOpen] = useState(false);
+  const openProductDetail = () => setIsProductOpen(true);
+  const closeProductDetail = () => setIsProductOpen(false);
   //console.log(count);
   return (
     <ShoppingCartContext.Provider
       value={{
         count,
         setCount,
+        isProductOpen,
+        openProductDetail,
+        closeProductDetail,
       }}
     >
       {children}
--- src/components/ProductDetail/index.jsx
+++ src/components/ProductDetail/index.jsx
@@ -1,11 +1,18 @@
 import { XMarkIcon } from '@heroicons/react/24/solid';
+import { ShoppingCartContext } from '../../Context';
+import { useContext } from 'react';
 
 const ProducDetail = () => {
+  const { isProductOpen, closeProductDetail } = useContext(ShoppingCartContext);
   return (
-    <aside className=' flex flex-col fixed right-0 border bg-white border-black rounded-lg w-[360px] h-[calc(100vh-80px)] bottom-4 top-68'>
+    <aside
+      className={` ${
+        isProductOpen ? 'flex' : 'hidden'
+      } flex-col fixed right-0 border bg-white border-black rounded-lg w-[360px] h-[calc(100vh-80px)] bottom-4 top-68`}
+    >
       <div className='flex justify-between items-center p-6'>
         <h2 className='font-medium text-xl'>Detail</h2>
-        <div>
+        <div onClick={() => closeProductDetail()}>
           <XMarkIcon className='h-6 w-6 text-black'></XMarkIcon>
         </div>
       </div>
