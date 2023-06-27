3e3083bfd6891eb41e05b617ea1068e27ab3067f
Diego-caviedes <diego_1996.c.c@hotmail.com>
2023-06-26 21:25:24 -0500

    creando pagina de la lista de ordenes

--- src/Pages/MyOrder/MyOrder.jsx
+++ src/Pages/MyOrder/MyOrder.jsx
@@ -2,13 +2,19 @@ import React, { useContext } from 'react';
 import Layout from '../../components/Layout';
 import { ShoppingCartContext } from '../../Context';
 import OrderCard from '../../components/OrderCard';
+import { ChevronLeftIcon } from '@heroicons/react/24/solid';
+import { Link } from 'react-router-dom';
 
 function MyOrder() {
   const { order } = useContext(ShoppingCartContext);
-  console.log(order?.slice(-1)[0]);
   return (
     <Layout>
-      MyOrder
+      <div className='flex items-center justify-center relative w-80 mb-6'>
+        <Link to='/my-orders' className='absolute left-0'>
+          <ChevronLeftIcon className='h-6 w-6 text-black cursor-pointer'></ChevronLeftIcon>
+        </Link>
+        <h1>My Order</h1>
+      </div>
       <div className='flex flex-col w-80'>
         {order?.slice(-1)[0].products.map((product) => (
           <OrderCard
--- src/Pages/MyOrders/MyOrders.jsx
+++ src/Pages/MyOrders/MyOrders.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import OrdersCard from '../../OrdersCard';
+import { ShoppingCartContext } from '../../Context';
+import { Link } from 'react-router-dom';
+import Layout from '../../components/Layout';
+import { ChevronLeftIcon } from '@heroicons/react/24/solid';
 
 function MyOrders() {
+  const { order } = useContext(ShoppingCartContext);
+
   return (
     <Layout>
-      MyOrders
+      <div className='flex items-center  relative w-80'>
+        <h1>My Orders</h1>
+      </div>
+      {order.map((order, index) => {
+        <Link key={index} to={`/my-orders/${order.id}`}>
+          <OrdersCard
+            totalPrice={order.totalPrice}
+            totalProducts={order.totalProducts}
+          />
+        </Link>;
+      })}
     </Layout>
-  )
+  );
 }
 
-export default MyOrders
+export default MyOrders;
